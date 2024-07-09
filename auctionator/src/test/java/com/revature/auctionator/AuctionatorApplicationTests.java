f4a3b934650730b4368d4f8bf1c57f34f90e744e
ResidentTenko <109637465+ResidentTenko@users.noreply.github.com>
2024-07-09 17:58:23 -0400

    add project files to repo

---
+++ auctionator/src/test/java/com/revature/auctionator/AuctionatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.auctionator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
