574b43dc7de1fb249376d611200bd52c4d6cada4
Teddy Solomon <xzl896@mocs.utc.edu>
2024-07-10 09:24:05 -0500

    add models package

---
+++ auctionator/src/main/java/com/revature/models/User.java
@@ -0,0 +1,18 @@
+package com.revature.models;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "u_id", updatable = false)
+    private int id;
+
+
+}
