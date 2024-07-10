c9706e50cec5bdf59935a1feeec297a6861d4a23
ResidentTenko <109637465+ResidentTenko@users.noreply.github.com>
2024-07-10 16:49:09 -0400

    Add files via upload

---
+++ sql-scripts/data.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS auctions;
+DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users(
+	u_id serial PRIMARY KEY,
+	first_name varchar(50),
+	last_name varchar(50),
+	username varchar(50) UNIQUE,
+	password varchar(50),
+	balance decimal(10, 2),
+	u_role varchar(50) DEFAULT 'Client'
+);
+
+CREATE TABLE items(
+	i_id serial PRIMARY KEY,
+	item_name varchar(50),
+	sold boolean DEFAULT FALSE,
+	owner_id int REFERENCES users(u_id) ON DELETE SET NULL
+);
+
+CREATE TABLE auctions(
+	a_id serial PRIMARY KEY,
+	auction_item_id int REFERENCES items(i_id) ON DELETE SET NULL,
+	bid decimal(10, 2),
+	bidder_id int REFERENCES users(u_id) ON DELETE SET NULL,
+	status varchar(50) DEFAULT 'Inactive',
+	a_time int
+);
+
+CREATE TABLE comments(
+	c_id serial PRIMARY KEY,
+	commenter_id int REFERENCES users(u_id) ON DELETE SET NULL,
+	auction_id int REFERENCES auctions(a_id) ON DELETE SET NULL,
+	comment varchar(255)
+);
