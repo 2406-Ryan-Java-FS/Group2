cfac2d67954dc37813d8b87a7affd21570a74240
c-Harrod <101145026+c-Harrod@users.noreply.github.com>
2024-07-10 16:42:28 -0400

    Update CommentRepository.java

--- auctionator/src/main/java/com/revature/auctionator/repositories/CommentRepository.java
+++ auctionator/src/main/java/com/revature/auctionator/repositories/CommentRepository.java
@@ -2,6 +2,9 @@ package com.revature.auctionator.repositories;
 
 import com.revature.auctionator.models.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
 }
