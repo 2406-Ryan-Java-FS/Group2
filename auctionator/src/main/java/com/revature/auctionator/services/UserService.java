4f8a8a7203e6e71bae7b162d36a5294577e6b73d
Teddy Solomon <xzl896@mocs.utc.edu>
2024-07-10 16:28:58 -0500

    UserServiceImpl complete

--- auctionator/src/main/java/com/revature/auctionator/services/UserService.java
+++ auctionator/src/main/java/com/revature/auctionator/services/UserService.java
@@ -1,4 +1,14 @@
 package com.revature.auctionator.services;
 
+import com.revature.auctionator.models.User;
+
+import java.util.List;
+
 public interface UserService {
+
+    public User addUser(User u);
+    public User getUser(int id);
+    public List<User> getAllUsers();
+    public User updateUser(User change);
+    public boolean deleteUser(int id);
 }
--- auctionator/src/main/java/com/revature/auctionator/services/UserServiceImpl.java
+++ auctionator/src/main/java/com/revature/auctionator/services/UserServiceImpl.java
@@ -1,12 +1,49 @@
 package com.revature.auctionator.services;
 
+import com.revature.auctionator.models.User;
 import com.revature.auctionator.repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     UserRepo ur;
+
+    @Override
+    public User addUser(User u) {
+        return ur.save(u);
+    }
+
+    @Override
+    public User getUser(int id) {
+        return ur.findById(id).orElseGet(User::new);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return ur.findAll();
+    }
+
+    @Override
+    public User updateUser(User change) {
+        return ur.save(change);
+    }
+
+    @Override
+    public boolean deleteUser(int id) {
+        try {
+            User u = getUser(id);
+            if(u.getId() != 0) {
+                ur.deleteById(id);
+                return true;
+            }
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
