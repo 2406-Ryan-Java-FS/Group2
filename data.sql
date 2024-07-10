29a06ea93f159d41e3b249e75189b2b8b3a924ab
ResidentTenko <109637465+ResidentTenko@users.noreply.github.com>
2024-07-10 00:40:27 -0400

    adds on delete set null property

--- data.sql
+++ data.sql
@@ -4,10 +4,10 @@ DROP TABLE IF EXISTS items;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users(
-	u_id serial primary KEY,
+	u_id serial PRIMARY KEY,
 	first_name varchar(50),
 	last_name varchar(50),
-	username varchar(50) unique,
+	username varchar(50) UNIQUE,
 	password varchar(50),
 	balance decimal(10, 2),
 	u_role varchar(50)
@@ -17,21 +17,21 @@ CREATE TABLE items(
 	i_id serial PRIMARY KEY,
 	item_name varchar(50),
 	sold boolean DEFAULT FALSE,
-	owner_id int REFERENCES users(u_id)
+	owner_id int REFERENCES users(u_id) ON DELETE SET NULL
 );
 
 CREATE TABLE auctions(
 	a_id serial PRIMARY KEY,
-	item_id int REFERENCES items(i_id),
+	item_id int REFERENCES items(i_id) ON DELETE SET NULL,
 	bid decimal(10, 2),
-	bidder_id int REFERENCES users(u_id),
+	bidder_id int REFERENCES users(u_id) ON DELETE SET NULL,
 	status varchar(50) DEFAULT 'Inactive',
 	a_time int
 );
 
 CREATE TABLE comments(
 	c_id serial PRIMARY KEY,
-	commenter_id int REFERENCES users(u_id),
-	auction_id int REFERENCES auctions(a_id),
+	commenter_id int REFERENCES users(u_id) ON DELETE SET NULL,
+	auction_id int REFERENCES auctions(a_id) ON DELETE SET NULL,
 	comment varchar(255)
 );
