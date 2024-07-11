82ad66d1e1fba935203203575041a23cf107d300
Eric-Ro <projectrobertson@gmail.com>
2024-07-11 10:44:20 -0500

    added basic functionality to auction layers

--- auctionator/src/main/java/com/revature/auctionator/services/AuctionService.java
+++ auctionator/src/main/java/com/revature/auctionator/services/AuctionService.java
@@ -1,4 +1,9 @@
 package com.revature.auctionator.services;
 
+import com.revature.auctionator.models.Auction;
+
+import java.util.List;
+
 public interface AuctionService {
+    List<Auction> findAllAuctions();
 }
--- auctionator/src/main/java/com/revature/auctionator/services/AuctionServiceImpl.java
+++ auctionator/src/main/java/com/revature/auctionator/services/AuctionServiceImpl.java
@@ -1,7 +1,14 @@
 package com.revature.auctionator.services;
 
+import com.revature.auctionator.repositories.AuctionRepo;
+import com.revature.auctionator.repositories.ItemRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuctionServiceImpl implements AuctionService {
+    private AuctionRepo auctionRepo;
+
+    public AuctionServiceImpl(AuctionRepo auctionRepo) {
+        this.auctionRepo = auctionRepo;
+    }
 }
