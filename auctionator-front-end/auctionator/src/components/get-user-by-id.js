6d725ca6d551b93acff65237f91c1510e5e9ba96
Teddy Solomon <xzl896@mocs.utc.edu>
2024-07-18 09:49:21 -0500

    Update get-user-by-id

--- auctionator-front-end/auctionator/src/components/get-user-by-id.js
+++ auctionator-front-end/auctionator/src/components/get-user-by-id.js
@@ -1,6 +1,15 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import styles from "../styles/user.module.css";
 
 export default function GetUserById() {
+    let [foundUser, setFoundUser] = useState({
+        firstName: null,
+        lastName: null,
+        username: null,
+        password: null,
+        balance: null,
+        role: null
+    });
     const userIdInput = useRef();
 
     async function getUser() {
@@ -11,13 +20,15 @@ export default function GetUserById() {
             try {
                 const httpResponse = await fetch(url);
                 const body = await httpResponse.json();
-                alert("First Name: " + body['firstName'] + 
-                    "\nLast Name: " + body['lastName'] +
-                    "\nUserame: " + body['username'] +
-                    "\nPassword: " + body['password'] +
-                    "\nBalance: " + body['balance'] +
-                    "\nRole: " + body['role']
-                );
+                console.log(body);
+                setFoundUser(body);
+                // alert("First Name: " + body['firstName'] + 
+                //     "\nLast Name: " + body['lastName'] +
+                //     "\nUserame: " + body['username'] +
+                //     "\nPassword: " + body['password'] +
+                //     "\nBalance: " + body['balance'] +
+                //     "\nRole: " + body['role']
+                // );
             } catch (error) {
                 alert("User with that ID not found.");
             }
@@ -27,7 +38,29 @@ export default function GetUserById() {
     return(<>
         <label htmlFor="id">Enter ID: </label>
         <input type='number' id="id" ref={userIdInput} /><br/>
-        <button onClick={getUser}>Find User</button>
+        <button onClick={getUser}>Find User</button><br/>
+        <table style={{border: '1px solid black'}} className={styles.userTable}>
+            <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Username</th>
+                    <th>Password</th>
+                    <th>Balance</th>
+                    <th>Role</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{foundUser['firstName']}</td>
+                    <td>{foundUser['lastName']}</td>
+                    <td>{foundUser['username']}</td>
+                    <td>{foundUser['password']}</td>
+                    <td>{foundUser['balance']}</td>
+                    <td>{foundUser['role']}</td>
+                 </tr>
+            </tbody>
+        </table>
         
     </>)
 }
