017aee432c4fe723b77bd3b8d72978fdeee70e55
Teddy Solomon <xzl896@mocs.utc.edu>
2024-07-18 16:57:42 -0500

    Adding more routes

--- auctionator-front-end/auctionator/src/App.js
+++ auctionator-front-end/auctionator/src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import StartPage from './components/start-page';
 import UserLogin from './components/user-login';
 import UserSignup from './components/user-signup';
+import ClientPage from './components/client-page';
+import AdminPage from './components/admin-page';
 import UserLogout from './components/user-logout';
 import UpdateUserBalance from './components/update-user-balance';
 import UpdateUserInfo from './components/update-info';
@@ -24,6 +26,12 @@ function App() {
       <Route path='' element = {<StartPage />} />
       <Route path='signup' element = {<UserSignup />} />
       <Route path='login' element = {<UserLogin />} />
+      <Route path='client' element = {<ClientPage />}/>
+      <Route path='admin' element = {<AdminPage />}/>
+      <Route path='userlist' element = {<GetAllUsers />}/>
+      <Route path='finduser' element = {<GetUserById />}/>
+      <Route path='setbalance' element = {<UpdateUserBalance />}/>
+      <Route path='setinfo' element = {<UpdateUserInfo />}/>
     </Routes>
   </>);
 }
