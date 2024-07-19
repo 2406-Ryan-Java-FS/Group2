1412f4a9c7794ba800feea6bd83a3db3bc9a0d9e
ResidentTenko <109637465+ResidentTenko@users.noreply.github.com>
2024-07-19 12:20:15 -0400

    Updates the backend without node_modules.

--- auctionator/src/main/java/com/revature/auctionator/repositories/ItemRepository.java
+++ auctionator/src/main/java/com/revature/auctionator/repositories/ItemRepository.java
@@ -1,13 +1,24 @@
 package com.revature.auctionator.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.revature.auctionator.models.Item;
+import com.revature.auctionator.models.ItemUserDTO;
 
 // entity type: Item
 // primary key: Integer
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Integer>{
 
+    @Query("SELECT new com.revature.auctionator.models.ItemUserDTO(i.id, i.itemName, i.sold, u.firstName, u.lastName, u.username, i.ownerId) " +
+           "FROM Item i INNER JOIN User u ON i.ownerId = u.id")
+    List<ItemUserDTO> findAllItemsAndUserInfo();
+
+    @Query("SELECT new com.revature.auctionator.models.ItemUserDTO(i.id, i.itemName, i.sold, u.firstName, u.lastName, u.username, i.ownerId) " +
+           "FROM Item i INNER JOIN User u ON i.ownerId = u.id WHERE u.username = :username")
+    List<ItemUserDTO> findAllItemsByUsername(String username);
 }
--- auctionator/src/main/java/com/revature/auctionator/services/ItemService.java
+++ auctionator/src/main/java/com/revature/auctionator/services/ItemService.java
@@ -3,11 +3,15 @@ package com.revature.auctionator.services;
 import java.util.List;
 
 import com.revature.auctionator.models.Item;
+import com.revature.auctionator.models.ItemUserDTO;
 
 public interface ItemService {
     List<Item> findAllItems();
+    List<ItemUserDTO> findAllItemsAndUserInfo();
+    List<ItemUserDTO> findAllItemsByUsername(String username);
     Item findItemById(int theItemId);
     Item addNewItem(Item theNewItem);
+    Item addNewItemByUsername(Item theNewItem, String username);
     Item updateItemById(Item theitem, int theItemId);
     int deleteItemById (int theItemId);
 }
