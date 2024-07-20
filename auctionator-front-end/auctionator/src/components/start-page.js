7ac9df345f52a2c023027f50adb3c4b331686565
c-Harrod <101145026+c-Harrod@users.noreply.github.com>
2024-07-20 16:17:50 -0400

    Update start-page.js

--- auctionator-front-end/auctionator/src/components/start-page.js
+++ auctionator-front-end/auctionator/src/components/start-page.js
@@ -2,12 +2,33 @@ import { Link } from "react-router-dom";
 
 export default function StartPage() {
     return (<>
-        <h1>Welcome to Auctionator!</h1>
-        <Link to="/signup">
-            <button>Sign Up</button>
-        </Link>
-        <Link to="/login">
-            <button>Log In</button>
-        </Link>
+        <div className="container mt-5">                  
+            <div className="card">
+                <div className="card-header text-center fw-bold fs-3">
+                    Welcome to the Auctionator!
+                </div>
+                <div className="card-body">
+                    <div className="row">
+                        <div className="d-flex justify-content-center align-items-center col-12">
+                            <div className="d-flex justify-content-center col-3">
+                                <Link to="/login">
+                                    <button className="btn btn-primary">Log In</button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                    <br />
+                    <div className="row">
+                        <div className="d-flex justify-content-center align-items-center col-12">
+                            <div className="d-flex justify-content-center col-3">
+                                <Link to="/signup">
+                                    <button className="btn btn-primary">Sign Up</button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> 
     </>);
 }
